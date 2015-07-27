c4e2db406f27a9f83e5eebcd7bb345364bbc2de5
rimaaii <rgerhard@aii.edu>
2015-07-27 14:00:37 -0400

    Create script.js

---
+++ debug-lab/script.js
@@ -0,0 +1,7 @@
+<p>  
+alter("Hello World");
+document.write("<h1>Hello World</h1>);
+documen.write("<p>Today I'm going to debug.");
+document.write("<p><img src="http://phdcomics.com/comics/archive/phd011406s.gif">");
+document.write("<p>Developer Tools to the rescue!");
+alert("I am the Master of Debugging!);
